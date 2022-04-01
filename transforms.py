1a3f6f046da06da8df453f0fd367fdb87cba5cfb
MasatoNagoya <morita.masato.s3@s.mail.nagoya-u.ac.jp>
2022-04-01 17:57:53 +0900

    modify transforms.py

--- transforms.py
+++ transforms.py
@@ -2,6 +2,7 @@ from torchvision import transforms
 import albumentations as alb
 from albumentations.pytorch import ToTensorV2
 
+
 def get_train_transform() -> alb.Compose:
     '''
     画像にどのような変形を加えるか(後にDatasetオブジェクトに渡す)
@@ -10,7 +11,10 @@ def get_train_transform() -> alb.Compose:
         # Tensor型に変換
         ToTensorV2()
         # その他処理があれば記述する。
-    ])
+
+    ],
+        bbox_params={'format': 'pascal_voc', 'label_fields': ['labels']}
+    )
 
 
 def get_valid_transform() -> alb.Compose:
@@ -21,4 +25,6 @@ def get_valid_transform() -> alb.Compose:
         # Tensor型に変換
         ToTensorV2()
         # その他処理があれば記述する。
-    ])
+    ],
+        bbox_params={'format': 'pascal_voc', 'label_fields': ['labels']}
+    )
