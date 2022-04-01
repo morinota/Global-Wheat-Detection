0803f925b85535cb21b5553cb22ff75a3aba9989
MasatoNagoya <morita.masato.s3@s.mail.nagoya-u.ac.jp>
2022-04-01 17:49:54 +0900

    arrange

--- transforms.py
+++ transforms.py
@@ -1,21 +1,24 @@
 from torchvision import transforms
-import albumentations as A
-def get_train_transform()->transforms.Compose:
+import albumentations as alb
+from albumentations.pytorch import ToTensorV2
+
+def get_train_transform() -> alb.Compose:
     '''
     画像にどのような変形を加えるか(後にDatasetオブジェクトに渡す)
     '''
-    return transforms.Compose([
+    return alb.Compose([
         # Tensor型に変換
-        transforms.ToTensor()
+        ToTensorV2()
         # その他処理があれば記述する。
     ])
 
-def get_valid_transform()->transforms.Compose:
+
+def get_valid_transform() -> alb.Compose:
     '''
     画像にどのような変形を加えるか(後にDatasetオブジェクトに渡す)
     '''
-    return transforms.Compose([
+    return alb.Compose([
         # Tensor型に変換
-        transforms.ToTensor()
+        ToTensorV2()
         # その他処理があれば記述する。
     ])
