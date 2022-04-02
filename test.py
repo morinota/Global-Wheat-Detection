d6a481e063cfedf3fe9eabbd22c49f1076ab08d5
MasatoNagoya <morita.masato.s3@s.mail.nagoya-u.ac.jp>
2022-04-02 18:12:52 +0900

    change lr

--- test.py
+++ test.py
@@ -4,6 +4,9 @@ from torchvision.models.detection.faster_rcnn import FasterRCNN
 
 
 def show(val_dataloader: DataLoader, model: FasterRCNN):
+    '''
+    推論結果をチェックする関数
+    '''
     import matplotlib.pyplot as plt
     from PIL import ImageDraw, ImageFont
     from PIL import Image
@@ -17,3 +20,5 @@ def show(val_dataloader: DataLoader, model: FasterRCNN):
     model.to(device)
     # modelを推論モードへ
     model.eval()
+
+    # modelに画像のTensorを渡せば推論を実行する(返値もTensor)
