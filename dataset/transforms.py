5de03d1644e128934f7be077abd21b018471079e
MasatoNagoya <morita.masato.s3@s.mail.nagoya-u.ac.jp>
2022-04-06 17:04:38 +0900

    aa

--- dataset/transforms.py
+++ dataset/transforms.py
@@ -8,7 +8,6 @@ def get_train_transform() -> alb.Compose:
     画像にどのような変形を加えるか(後にDatasetオブジェクトに渡す)
     '''
     return alb.Compose([
-        transforms.Resize(size=)
         # Tensor型に変換
         ToTensorV2(), 
         # その他処理があれば記述する。
